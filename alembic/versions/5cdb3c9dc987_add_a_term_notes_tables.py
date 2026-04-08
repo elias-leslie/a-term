37e0362d404b905a83c46d4daec135147cd83e6b
SummitFlow Dev <kasadis@summitflow.dev>
2026-04-08 14:24:45 +0000

    chore: smart-sync - 2 files changed, 5 insertions(+), 6 deletions(-)

--- alembic/versions/5cdb3c9dc987_add_a_term_notes_tables.py
+++ alembic/versions/5cdb3c9dc987_add_a_term_notes_tables.py
@@ -7,10 +7,10 @@ Create Date: 2026-04-06 16:41:21.976586
 """
 from collections.abc import Sequence
 
-from alembic import op
 import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "5cdb3c9dc987"
--- alembic/versions/af0db560ec3f_normalize_a_term_note_scopes.py
+++ alembic/versions/af0db560ec3f_normalize_a_term_note_scopes.py
@@ -5,16 +5,15 @@ Revises: 5cdb3c9dc987
 Create Date: 2026-04-06 18:10:34.186481
 
 """
-from typing import Sequence, Union
+from collections.abc import Sequence
 
 from alembic import op
 
-
 # revision identifiers, used by Alembic.
 revision: str = 'af0db560ec3f'
-down_revision: Union[str, Sequence[str], None] = '5cdb3c9dc987'
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+down_revision: str | Sequence[str] | None = '5cdb3c9dc987'
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
